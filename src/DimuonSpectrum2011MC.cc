db5d88ca9c7f0675da9f521c1d04db956ce636d9
FQMeng <mengfanqiang@pku.edu.cn>
2020-04-25 17:28:10 +0800

    Update DimuonSpectrum2011MC.cc

--- src/DimuonSpectrum2011MC.cc
+++ src/DimuonSpectrum2011MC.cc
@@ -427,11 +427,11 @@ void DimuonSpectrum2011::endJob() {
 bool DimuonSpectrum2011::eta21pt1510 (double eta1, double eta2, double pt1, double pt2){
   if (fabs(eta1) < 2.1 && fabs(eta2) < 2.1){
     if ((pt1 > 10 && pt2 > 10) && (pt1 > 15 || pt2 > 15)){
-      return true
+      return true;
     }
   }
   else
-    return false
+    return false;
 }
 
 //define this as a plug-in
